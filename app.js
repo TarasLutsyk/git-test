5b2dd596eb39033fe266c658ca08d282be7f1006
ItWarior <77105647+ItWarior@users.noreply.github.com>
2022-03-28 12:59:00 +0300

    chenge

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 console.log('new section');
 
-let dfd = 100
-let dfdsto = 100
-let sdfto = 100
-let sdftso = 100
+function soele() {
+    console.log('some')
 
+}
 let some = 25
 let bob = 25
 let enother = 'str'
