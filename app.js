8532fdf94fed1fba29f6807fa7c19e53d6d91796
ItWarior <77105647+ItWarior@users.noreply.github.com>
2022-03-30 19:24:47 +0300

    some

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 console.log('new section');
 
-function some() {
-    console.log('some')
-}
+let dfd = 100
+let dfdsto = 100
+let sdfto = 100
+let sdftso = 200
+
+let some = 25
+let bob = 25
+let enother = 'str'
+let ager = 25
