22d6c2a8b606a5e4f8cdbcc3315de26e979eae97
ItWarior <77105647+ItWarior@users.noreply.github.com>
2022-03-30 19:13:02 +0300

    new

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 console.log('new section');
 
-let dfd = 100
-let dfdsto = 100
-let sdfto = 100
-let sdftso = 200
-
-let some = 25
-let bob = 25
-let enother = 'str'
-let ager = 25
+function some() {
+    console.log('some')
+}
